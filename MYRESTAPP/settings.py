f1c7be71b187978b15ef1812bc4e342d4f351182
despean <despean15@gmail.com>
2017-04-25 16:15:33 +0100

    Update settings.py

--- MYRESTAPP/settings.py
+++ MYRESTAPP/settings.py
@@ -78,13 +78,13 @@ WSGI_APPLICATION = 'MYRESTAPP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'simpleapi',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {'charset': 'utf8mb4'}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'OPTIONS': {'charset': 'utf8mb4'}
     }
 }
 
